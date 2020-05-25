188cd25c1b6aad59e760078c6f38f6a219423dd6
Ole Kröger <o.kroeger@opensourc.es>
2020-05-25 14:40:25 +0200

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # ProjectEulerDownload
+
+Makes it easy to download a problem from [Project Euler](https://projecteuler.net/) into a nice folder structure to solve them one by one.
+
+## Installation
+
+This is not a registered package so you need to add it with:
+
+```
+] add https://github.com/Wikunia/ProjectEulerDownload.jl
+```
+
+## Usage
+
+```
+using ProjectEulerDownload
+# to have a shorter notation
+const PED = ProjectEulerDownload
+
+# once =>
+PED.configure("PATH TO YOUR PROJECT EULER ROOT DIRECTORY")
+
+# then
+PED.download(1)
+```
+
+will create a folder `001_Multiples_of_3_and_5` in your specified directory with three files:
+
+#### README.md
+
+Includes the problem description and a link to the actual problem.
+
+#### solution.jl
+
+Just an empty file where you can define your functions.
+
+#### runtests.jl
+
+Includes:
+
+```
+using Test
+
+include("solution.jl")
+
+@testset "example" begin
+	@test
+end
+```
+
+here you can define your own test sets which can help you to solve the problem.
+
+## Disclaimer
+
+I have absolutely nothing to do with ProjectEuler. I just like to solve their problems ;)
--- src/ProjectEulerDownload.jl
+++ src/ProjectEulerDownload.jl
@@ -1,5 +1,68 @@
 module ProjectEulerDownload
 
-greet() = print("Hello World!")
+using JSON
+using Cascadia
+using DataFrames
+using Gumbo
+using HTTP
 
-end # module
+const PED = ProjectEulerDownload
+
+function configure(euler_dir::String)
+    dir = pkgdir(PED)
+    d = Dict("root" => euler_dir)
+    write(joinpath(dir, "config.json"), JSON.json(d))
+end
+
+function download(problem_id::Int)
+    pdir = pkgdir(PED)
+    config_path = joinpath(pdir, "config.json")
+    if !isfile(config_path) 
+        error("Please run PED.configure(\"Path to root directory\") first") 
+        return
+    end
+    root_dir = JSON.parsefile(config_path)["root"]
+    cd(root_dir)
+
+    url = "https://projecteuler.net/problem=$problem_id";
+    res = HTTP.get(url);
+    body = String(res.body);
+    html = parsehtml(body);
+    
+    title = text(eachmatch(sel"h2", html.root)[1][1]);
+    folder_title = replace(title, " "=> "_")
+    lid = lpad(problem_id, 3, '0')
+    folder = "$(lid)_$folder_title"
+    # create the new folder and change to there
+    mkdir(folder)
+    cd(folder)
+
+    # CREATE SEVERAL FILES
+    # README.md
+    problem_content = eachmatch(sel".problem_content", html.root)[1]
+    str_content = string(problem_content)
+    str_content = replace(str_content, "</p>" =>"\n")
+    str_content = replace(str_content, r"<dfn(>|.*?>)(?<text>.*?)</dfn>" => s"**\g<text>**")
+    str_content = replace(str_content, r"<(?<tag>[/]?(sup|sub))>" => s"<_\g<tag>>")
+    str_content = replace(str_content, r"<[^_].*?>" => "")
+    str_content = replace(str_content, r"<_(?<tag>.*?)>" => s"<\g<tag>>")
+
+    readme = "# $title\n$str_content\n\nSource: [Project Euler #$problem_id]($url)"
+    write("README.md", readme)
+    println("Checkout the readme!")
+    println(joinpath(pwd(), "README.md"))
+
+    # solution.jl
+    write("solution.jl", "")
+
+    # runtests.jl
+    test_file = "using Test\n\ninclude(\"solution.jl\")\n\n"
+    test_file *= "@testset \"example\" begin\n"
+    test_file *= "\t@test\n"
+    test_file *= "end"
+    write("runtests.jl", test_file);
+
+    println("Happy coding!")
+end
+
+end
