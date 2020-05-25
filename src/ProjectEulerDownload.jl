b69d1eceee48af726436180ee77fc63ac8507326
Ole Kröger <o.kroeger@opensourc.es>
2020-05-25 12:09:15 +0200

    Files generated by PkgTemplates

---
+++ src/ProjectEulerDownload.jl
@@ -0,0 +1,5 @@
+module ProjectEulerDownload
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ProjectEulerDownload
+using Test
+
+@testset "ProjectEulerDownload.jl" begin
+    # Write your own tests here.
+end
